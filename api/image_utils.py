3c7cb71c218bd1f2ec31760494f2bbc54230d2ab
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-09-10 16:15:54 +0200

    add method for converting image and saving directly to disk

--- api/image_utils.py
+++ api/image_utils.py
@@ -1,8 +1,11 @@
 import base64
 import hashlib
 from io import BytesIO
+import os
 from typing import Union
 from PIL import Image
+
+from .classes import ImageMetadata
 
 
 class ImageUtils:
@@ -56,6 +59,39 @@ class ImageUtils:
         buf.seek(0)
         new_image = Image.open(buf)
         return new_image
+
+    @staticmethod
+    def convert_to_unified_format_and_write_to_filesystem(output_path: str, image: Image.Image) -> tuple[str, ImageMetadata]:
+        """
+        Generates a new image from an input image with the following properties:
+        - RGB color palette (no alpha channel)
+        - PNG format
+        - Maximum size: 2048 x 2048
+        - no EXIF data from the input image
+
+        Args:
+            output_path (str): the path to write the image to (filename will be appended)
+            image (PIL.Image.Image): the image to convert
+        """
+        
+        FORMAT = "png"
+        rgb_image = image.convert("RGB")
+
+        max_size = 2048
+
+        if rgb_image.width > max_size or rgb_image.height > max_size:
+            rgb_image = ImageUtils.resize(rgb_image, max_size, max_size, copy=False)
+                
+        os.makedirs(output_path, exist_ok=True)
+        
+        id = ImageUtils.get_id(rgb_image)        
+        filename = os.path.join(output_path, ImageUtils.get_filename(id=id, width=rgb_image.width, height=rgb_image.height, format=FORMAT))
+        rgb_image.save(filename, format=FORMAT)
+        
+        metadata = ImageMetadata(original_width=rgb_image.width, original_height=rgb_image.height, media_type=Image.MIME.get(FORMAT.upper()), format=FORMAT)
+        
+        return (id, metadata)
+        
     
     @staticmethod
     def get_filename_with_image_data(*, id: str, data: Image.Image):
