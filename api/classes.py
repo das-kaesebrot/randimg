5e60209e40a9c26822d94bbcd1895d98f6edf4d4
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-09-10 17:32:49 +0200

    drop cachedimage class

--- api/classes.py
+++ api/classes.py
@@ -1,37 +1,9 @@
 from dataclasses import dataclass
 from typing import Union
 from fastapi.responses import Response
-import io
 from PIL import Image
 
-from .image_utils import ImageUtils
-
-
-@dataclass
-class CachedImage:
-    data: Image.Image
-    
-    @property
-    def width(self) -> int:
-        return self.data.width
-
-    @property
-    def height(self) -> int:
-        return self.data.height
-    
-    @property
-    def media_type(self) -> str:
-        return Image.MIME.get(self.data.format.upper())
-    
-    @property
-    def id(self) -> str:
-        return ImageUtils.get_id(data=self.data)
-    
-    def get_bytesio(self) -> io.BytesIO:
-        buf = io.BytesIO()
-        self.data.save(fp=buf, format=self.data.format)
-        buf.seek(0)
-        return buf
+from .filename_utils import FilenameUtils
 
 
 @dataclass
