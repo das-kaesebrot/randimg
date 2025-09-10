1463b482fe6feb8056268367fa0a2c1beeac7319
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-09-10 17:34:47 +0200

    refactor main and cache: utilize filesystem for writing cache files instead of holding images in memory

--- api/cache.py
+++ api/cache.py
@@ -2,18 +2,20 @@ import os
 import random
 from PIL import Image
 from typing import Dict, Union
-from .classes import CachedImage
+from .classes import ImageMetadata
+from .filename_utils import FilenameUtils
 from .image_utils import ImageUtils
+from .utils import Utils
 
 class Cache:
-    img_dict: Dict[str, CachedImage] = {}
+    _metadata_dict: Dict[str, ImageMetadata] = {}
+    _cache_dir: str
 
-    def __init__(self, image_dir: str):
-        self.img_dict = Cache._generate_cache(image_dir)
+    def __init__(self, *, image_dir: str, cache_dir: str):
+        self._cache_dir = cache_dir
+        self._generate_cache(image_dir)
         
-    
-    @staticmethod
-    def _generate_cache(image_dir: str) -> Dict[str, CachedImage]:
+    def _generate_cache(self, image_dir: str) -> Dict[str, ImageMetadata]:
         images: list[Image.Image] = []
         
         for filename in os.listdir(image_dir):
@@ -22,21 +24,40 @@ class Cache:
                 img.load()
                 images.append(img)
                 
-        image_dict = {}
+        metadata_dict = {}
                 
         for image in images:
-            image = ImageUtils.convert_to_unified_format(image)
-            cached_image = CachedImage(data=image)
-            image_dict[cached_image.id] = cached_image
+            id, metadata = ImageUtils.convert_to_unified_format_and_write_to_filesystem(output_path=self._cache_dir, image=image)
+            metadata_dict[id] = metadata
             
-        return image_dict
-
-    def get(self, id: str) -> Union[CachedImage, None]:
-        return self.img_dict.get(id)
+        self._metadata_dict = metadata_dict
+    
+    def get_filename_and_generate_copy_if_missing(self, id: str, width: Union[int, None] = None, height: Union[int, None] = None) -> str:
+        metadata = self._metadata_dict.get(id)
+        
+        if not width:
+            width = metadata.original_width
+        
+        if not height:
+            height = metadata.original_height
+            
+        width, height = Utils.clamp(width, 0, metadata.original_width), Utils.clamp(height, 0, metadata.original_height)        
+        width, height = ImageUtils.calculate_scaled_size(metadata.original_width, metadata.original_height, width=width, height=height)
+                    
+        filename = os.path.join(self._cache_dir, FilenameUtils.get_filename(id=id, width=width, height=height, format=metadata.format))
+        
+        if not os.path.isfile(filename):
+            source_filename = os.path.join(self._cache_dir, FilenameUtils.get_filename(id=id, width=metadata.original_width, height=metadata.original_height, format=metadata.format))
+            filename = ImageUtils.write_scaled_copy_from_source_filename_to_filesystem(id=id, source_filename=source_filename, output_path=self._cache_dir, width=width, height=height)
+        
+        return filename
     
     def get_random_id(self) -> str:
-        return random.choice(list(self.img_dict.keys()))
+        return random.choice(list(self._metadata_dict.keys()))
     
-    def get_random(self) -> tuple[str, CachedImage]:
-        return random.choice(list(self.img_dict.items()))
+    def get_random(self) -> tuple[str, ImageMetadata]:
+        return random.choice(list(self._metadata_dict.values()))
+    
+    def get_metadata(self, id: str) -> Union[ImageMetadata, None]:
+        return self._metadata_dict.get(id)
     
--- main.py
+++ main.py
@@ -1,36 +1,30 @@
-import io
+import os
 from typing import Union
 from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse, HTMLResponse, RedirectResponse
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from PIL.Image import Image
 
 from api.cache import Cache
-from api.classes import CachedImage, FaviconResponse
-from api.image_utils import ImageUtils
+from api.classes import FaviconResponse, ImageMetadata
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-cache = Cache("assets/images")
+ENV_PREFIX = "RANDIMG"
 
-def get_filename(image_id: str, image_data: Image):
-    return f"{image_id}_{image_data.width}x{image_data.height}.{image_data.format.lower()}"
+source_image_dir = os.getenv(f"{ENV_PREFIX}_IMAGE_DIR", "assets/images")
+cache_dir = os.getenv(f"{ENV_PREFIX}_CACHE_DIR", "cache")
 
-def return_and_resize_image(image_id: str, image: CachedImage, width: Union[int, None] = None, height: Union[int, None] = None, download: bool = False) -> StreamingResponse:
-    img_data = ImageUtils.resize(image.data, width, height)
-    
-    buf = io.BytesIO()
-    img_data.save(fp=buf, format=img_data.format)
-    buf.seek(0)
-    
+cache = Cache(image_dir=source_image_dir, cache_dir=cache_dir)
+
+def return_file(image_id: str, filename: str, metadata: ImageMetadata, download: bool = False) -> FileResponse:
     content_disposition = "inline"
     if download:
-        content_disposition = f'attachment; filename="{ImageUtils.get_filename_with_image_data(image_id, img_data)}"'
-    
-    return StreamingResponse(buf, media_type=image.media_type, headers={'Content-Disposition': content_disposition, 'X-Image-Id': f'{image_id}', "Cache-Control": "max-age=2592000, public, no-transform"})
+        content_disposition = f'attachment; filename="{filename}"'
+        
+    return FileResponse(path=filename, media_type=metadata.media_type, headers={'Content-Disposition': content_disposition, 'X-Image-Id': f'{image_id}', "Cache-Control": "max-age=2592000, public, no-transform"})
 
 @app.get("/favicon.ico", response_class=FaviconResponse)
 async def get_favicon():
@@ -40,22 +34,30 @@ async def get_favicon():
         + "</svg>"
     )
 
-@app.get("/", response_class=RedirectResponse)
-async def page_redirect_rand_image(request: Request):
-    return cache.get_random_id()
+@app.get("/", response_class=Union[HTMLResponse, RedirectResponse])
+async def page_redirect_rand_image(request: Request, redirect: bool = False):
+    image_id = cache.get_random_id()
+    
+    if redirect:
+        return RedirectResponse(image_id)
+    
+    filename = cache.get_filename_and_generate_copy_if_missing(image_id)
+    return templates.TemplateResponse(request=request, name="image.html", context={"image_id": image_id, "image_filename": filename, "height": 512})
 
 @app.get("/{image_id}", response_class=HTMLResponse)
 async def page_get_image(request: Request, image_id: str):
-    image = cache.get(image_id)
-    filename = ImageUtils.get_filename_with_image_data(image_id=image_id, image_data=image.data)
+    filename = cache.get_filename_and_generate_copy_if_missing(image_id)
     return templates.TemplateResponse(request=request, name="image.html", context={"image_id": image_id, "image_filename": filename, "height": 512})
 
 @app.get("/api/img/{image_id}")
 async def api_get_image(image_id: str, width: Union[int, None] = None, height: Union[int, None] = None, download: bool = False):
-    image = cache.get(image_id)    
-    return return_and_resize_image(image_id, image, width, height, download)
+    filename = cache.get_filename_and_generate_copy_if_missing(image_id, width=width, height=height)
+    metadata = cache.get_metadata(image_id)
+    return return_file(image_id, filename, metadata, download)
 
 @app.get("/api/img")
 async def api_get_rand_image(width: Union[int, None] = None, height: Union[int, None] = None, download: bool = False):
-    image_id, image = cache.get_random()    
-    return return_and_resize_image(image_id, image, width, height, download)
+    image_id = cache.get_random_id()
+    filename = cache.get_filename_and_generate_copy_if_missing(image_id, width=width, height=height)
+    metadata = cache.get_metadata(image_id)
+    return return_file(image_id, filename, metadata, download)
