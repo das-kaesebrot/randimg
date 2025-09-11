b8e7f4b55f366db6a0c6eee652c5b3bb6553cf0b
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-09-11 16:13:43 +0200

    add basic logging to cache

--- api/cache.py
+++ api/cache.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import random
 from PIL import Image
@@ -7,16 +8,27 @@ from .filename_utils import FilenameUtils
 from .image_utils import ImageUtils
 from .utils import Utils
 
+from datetime import timedelta
+from time import perf_counter
+
 
 class Cache:
     _metadata_dict: Dict[str, ImageMetadata] = {}
     _cache_dir: str
+    _logger: logging.Logger
 
     def __init__(self, *, image_dir: str, cache_dir: str):
+        image_dir = os.path.abspath(image_dir)
+        cache_dir = os.path.abspath(cache_dir)
+        
+        self._logger = logging.getLogger(__name__)
+        self._logger.info(f"Created cache instance with image directory='{image_dir}' and cache directory='{cache_dir}'")
         self._cache_dir = cache_dir
         self._generate_cache(image_dir)
 
     def _generate_cache(self, image_dir: str) -> Dict[str, ImageMetadata]:
+        start = perf_counter()
+        
         images: list[Image.Image] = []
 
         for filename in os.listdir(image_dir):
@@ -25,19 +37,30 @@ class Cache:
                 or filename.lower().endswith(".jpeg")
                 or filename.lower().endswith(".png")
             ):
-                img = Image.open(os.path.join(image_dir, filename))
-                img.load()
-                images.append(img)
+                try:
+                    img = Image.open(os.path.join(image_dir, filename))
+                    img.load()
+                    images.append(img)
+                except OSError as e:
+                    self._logger.exception(f"Failed loading file '{os.path.join(image_dir, filename)}'")
+                    continue
 
         metadata_dict = {}
 
         for image in images:
-            id, metadata = ImageUtils.convert_to_unified_format_and_write_to_filesystem(
-                output_path=self._cache_dir, image=image
-            )
-            metadata_dict[id] = metadata
+            try:
+                id, metadata = ImageUtils.convert_to_unified_format_and_write_to_filesystem(
+                    output_path=self._cache_dir, image=image
+                )
+                metadata_dict[id] = metadata
+            except OSError:
+                self._logger.exception(f"Failed writing converted file '{'no filename' if not image.filename else image.filename}'")
+                continue
 
-        self._metadata_dict = metadata_dict
+        self._metadata_dict = metadata_dict    
+        end = perf_counter()
+        self._logger.info(f"Generated {len(self._metadata_dict.keys())} cached images in {timedelta(seconds=end-start)}")
+        
 
     def get_filename_and_generate_copy_if_missing(
         self, id: str, width: Union[int, None] = None, height: Union[int, None] = None
@@ -92,3 +115,6 @@ class Cache:
     
     def id_exists(self, id: str) -> bool:
         return id in self._metadata_dict.keys()
+    
+    def get_first_id(self) -> dict[str, ImageMetadata]:
+        return sorted(self._metadata_dict.keys())[0]
